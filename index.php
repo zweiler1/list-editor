e7153da2a87581f9e86ed7dc7778542c34cf1213
Marc Zweiler <marc.zweiler@outlook.at>
2024-07-08 00:09:37 +0200

    Removed embedded jquery file and made it being loaded via the web

--- index.php
+++ index.php
@@ -137,8 +137,6 @@
       <p class="copyright">&copy; 2024 Finger weg, des is meins.</p>
     </footer>
   </body>
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
-  <script src="./jQuery.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="./editor.js"></script>
 </html>
