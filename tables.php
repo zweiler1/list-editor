ab31a009a3c2c86ee8d358ce705d11da7458781b
Marc Zweiler <marc.zweiler@outlook.at>
2024-07-08 18:20:29 +0200

    Added functions to create cells based on their type and refactored the whole generation of tables

--- tables.php
+++ tables.php
@@ -7,88 +7,103 @@
   define('RED_LIGHT', "#B65151");
   define('RED_DARK', "#A44949");
 
-  function createRowFromData($id, &$data, $tableName, $isEditable) {
-    $data = (object) $data;
-    $isEven = $id % 2 == 0;
-    $grey = $isEven ? BACKGROUND_DARK : BACKGROUND_LIGHT;
-    $green = $isEven ? GREEN_DARK : GREEN_LIGHT;
-    $red = $isEven ? RED_DARK : RED_LIGHT;
-
-    $colorYear = $data->year != "-" ? $green : $red;
-    $colorType = $data->type != "-" ? $green : $red;
-    $colorName1 = empty($data->name1) || $data->name1 != "true" ? $red : $green;
-    $colorName2 = empty($data->name2) || $data->name2 != "true" ? $red : $green;
-    $colorName3 = empty($data->name3) || $data->name3 != "true" ? $red : $green;
-    $colorName4 = empty($data->name4) || $data->name4 != "true" ? $red : $green;
-    $colorName5 = empty($data->name5) || $data->name5 != "true" ? $red : $green;
-    $colorName6 = empty($data->name6) || $data->name6 != "true" ? $red : $green;
+  function createDescriptor($id, $row, $tableName, $content, $isEditable, $isEven) {
+    $color = $isEven ? BACKGROUND_DARK : BACKGROUND_LIGHT;
 
     if($isEditable) {
-      $name1 = empty($data->name1) ? "" : ($data->name1 == "true" ? "checked" : "");
-      $name2 = empty($data->name2) ? "" : ($data->name2 == "true" ? "checked" : "");
-      $name3 = empty($data->name3) ? "" : ($data->name3 == "true" ? "checked" : "");
-      $name4 = empty($data->name4) ? "" : ($data->name4 == "true" ? "checked" : "");
-      $name5 = empty($data->name5) ? "" : ($data->name5 == "true" ? "checked" : "");
-      $name6 = empty($data->name6) ? "" : ($data->name6 == "true" ? "checked" : "");
-
       return <<<EOL
-        <tr id="{$tableName}_entry_{$id}" class="row_entry">
-          <td class="row_cell_name" style="background-color: $grey">
-            <span class="row_text" id="{$tableName}_name_{$id}" contenteditable="true"  onchange="onChange({$tableName}_name_{$id})">{$data->name}</span>
-          </td>
-          <td class="row_cell_year" style="background-color: $colorYear">
-            <span class="row_text" id="{$tableName}_year_{$id}" contenteditable="true" onchange="onChange({$tableName}_year_{$id})">{$data->year}</span>
-          </td>
-          <td class="row_cell_type" style="background-color: $colorType">
-            <span class="row_text" id="{$tableName}_type_{$id}" contenteditable="true" onchange="onChange({$tableName}_type_{$id})">{$data->type}</span>
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName1" >
-            <input type="checkbox" id="{$tableName}_name1_{$id}" onchange="onValueChange({$tableName}_name1_{$id})" {$name1} />
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName2">
-            <input type="checkbox" id="{$tableName}_name2_{$id}" onchange="onValueChange({$tableName}_name2_{$id})" {$name2} />
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName3">
-            <input type="checkbox" id="{$tableName}_name3_{$id}" onchange="onValueChange({$tableName}_name3_{$id})" {$name3} />
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName4">
-            <input type="checkbox" id="{$tableName}_name4_{$id}" onchange="onValueChange({$tableName}_name4_{$id})"{$name4} />
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName5">
-            <input type="checkbox" id="{$tableName}_name5_{$id}" onchange="onValueChange({$tableName}_name5_{$id})"{$name5} />
-          </td>
-          <td class="row_cell_human" style="background-color: $colorName6">
-            <input type="checkbox" id="{$tableName}_name6_{$id}" onchange="onValueChange({$tableName}_name6_{$id})"{$name6} />
-          </td>
-        </tr>
-        EOL;
-    }
-    else {
-      $name1 = empty($data->name1) ? "-" : $data->name1;
-      $name2 = empty($data->name2) ? "-" : $data->name2;
-      $name3 = empty($data->name3) ? "-" : $data->name3;
-      $name4 = empty($data->name4) ? "-" : $data->name4;
-      $name5 = empty($data->name5) ? "-" : $data->name5;
-      $name6 = empty($data->name6) ? "-" : $data->name6;
-
+        <td class="cell_descriptor" style="background-color: $color">
+          <span class="row_text" id="{$tableName}_{$row}_{$id}" contenteditable="true" onchange="onChange({$tableName}_{$row}_{$id})">{$content}</span>
+        </td>
+      EOL;
+    } else {
       return <<<EOL
-      <tr id="{$tableName}_entry_{$id}" class="row_entry">
-        <td class="row_cell_name" id="{$tableName}_name_{$id}" style="background-color: $grey">{$data->name}</td>
-        <td class="row_cell_year" id="{$tableName}_year_{$id}" style="background-color: $colorYear">{$data->year}</td>
-        <td class="row_cell_type" id="{$tableName}_type_{$id}" style="background-color: $colorType">{$data->type}</td>
-        <td class="row_cell_human" id="{$tableName}_name1_{$id}" style="background-color: $colorName1">{$data->name1}</td>
-        <td class="row_cell_human" id="{$tableName}_name2_{$id}" style="background-color: $colorName2">{$data->name2}</td>
-        <td class="row_cell_human" id="{$tableName}_name3_{$id}" style="background-color: $colorName3">{$data->name3}</td>
-        <td class="row_cell_human" id="{$tableName}_name4_{$id}" style="background-color: $colorName4">{$data->name4}</td>
-        <td class="row_cell_human" id="{$tableName}_name5_{$id}" style="background-color: $colorName5">{$name5}</td>
-        <td class="row_cell_human" id="{$tableName}_name6_{$id}" style="background-color: $colorName6">{$name6}</td>
-      </tr>
+        <td class="cell_descriptor" id="{$tableName}_{$row}_{$id}" style="background-color: $color">{$content}</td>
       EOL;
     }
   }
 
+  function createTextField($id, $row, $tableName, $content, $isEditable, $isEven) {
+    $green = $isEven ? GREEN_DARK : GREEN_LIGHT;
+    $red = $isEven ? RED_DARK : RED_LIGHT;
+    $color = $content != "-" ? $green : $red;
+
+    if($isEditable) {
+      return <<<EOL
+        <td class="cell_textfield" style="background-color: $color">
+          <span class="row_text" id="{$tableName}_{$row}_{$id}" contenteditable="true" onchange="onChange({$tableName}_{$row}_{$id})">{$content}</span>
+        </td>
+      EOL;
+    } else {
+      return <<<EOL
+        <td class="cell_textfield" id="{$tableName}_{$row}_{$id}" style="background-color: $color">{$content}</td>
+      EOL;
+    }
+  }
+
+  function createCheckbox($id, $row, $tableName, $content, $isEditable, $isEven) {
+    $green = $isEven ? GREEN_DARK : GREEN_LIGHT;
+    $red = $isEven ? RED_DARK : RED_LIGHT;
+    $color = $content == "true" ? $green : $red;
+    $contentText = $content == "true" ? "true" : "-";
+    $contentState = $content == "true" ? "checked" : "";
+
+    if($isEditable) {
+      return <<<EOL
+        <td class="cell_checkbox" style="background-color: $color">
+          <input type="checkbox" id="{$tableName}_{$row}_{$id}" onchange="onValueChange({$tableName}_{$row}_{$id})" {$contentState} />
+        </td>
+      EOL;
+    } else {
+      return <<<EOL
+        <td class="cell_checkbox" id="{$tableName}_{$row}_{$id}" style="background-color: $color">{$contentText}</td>
+      EOL;
+    }
+  }
+
+  function createBodyCell($id, $row, $tableName, $type, $content, $isEditable, $isEven) {
+    switch($type) {
+      case "descriptor":
+        return createDescriptor($id, $row, $tableName, $content, $isEditable, $isEven);
+        break;
+      case "textfield":
+        return createTextField($id, $row, $tableName, $content, $isEditable, $isEven);
+        break;
+      case "checkbox":
+        return createCheckbox($id, $row, $tableName, $content, $isEditable, $isEven);
+        break;
+      default:
+        echo "Error: The given tpye '$type' is an unknown cell type!";
+        exit;
+      }
+  }
+
+  # The 'useFirstRow' option is uesed when creating empty rows from a small columns data set.
+  function createTableRow($tableName, $i, $columns, $isEditable, $useFirstRow) {
+    $row = "<tr id=\"{$tableName}_entry_{$i}\" class=\"row_entry\">";
+    for ($j = 0; $j < count($columns); $j++) {
+      if($useFirstRow) { 
+        $row .= createBodyCell($j, $i, $tableName, $columns[$j]->type, $columns[$j]->data[0], $isEditable, $i % 2 == 0);
+      } else {
+        $row .= createBodyCell($j, $i, $tableName, $columns[$j]->type, $columns[$j]->data[$i], $isEditable, $i % 2 == 0);
+      }
+    }
+    $row .= "</tr>";
+    return $row;
+  }
+
   function createTableRows($tableName, $columns, $isEditable) {
+    # To enable calling this function from the events.php file, a check was implemented to see
+    # if the $columns variable is null, if it is, it will be loaded from file
+    if($columns == null) {
+      $tableData = file_get_contents("./data/tables/$tableName.json", "$tableName.json");
+      $columns = json_decode($tableData)->columns;
+    }
     $rows = "";
+    $rowCount = count($columns[0]->data);
+    for ($i = 0; $i < $rowCount; $i++) {
+      $rows .= createTableRow($tableName, $i, $columns, $isEditable, false);
+    }  
     return $rows;
   }
 
