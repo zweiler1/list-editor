1f11193688c33d3d95925d3964d35df3d0e442ac
Marc Zweiler <marc.zweiler@outlook.at>
2024-07-09 01:26:39 +0200

    Removed wololo

--- tools/convert_csv_batch.sh
+++ tools/convert_csv_batch.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # $1 path to directory containing csv files
 # $2 path to output directory
 
-echo wololo
 [ ! -d "$1" ] || [ ! -d "$2" ] && echo "Specified directories do not exist!" >&2 && exit 1
-echo wololo2
 find "$1" -type f -name "*.csv" -exec sh -c './convert_csv_to_json.sh $0 '"$2" {} \;
 
 # Easy readable alternative
