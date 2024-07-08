f2a3c28ce56b2651be791397fe712a81262c447b
Marc Zweiler <marc.zweiler@outlook.at>
2024-07-08 18:08:16 +0200

    Changed the page title and icon

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listeneditor</title>
-    <link rel="icon" type="image/x-icon" href="film-reel-svgrepo-com.svg">
+    <title>list-editor</title>
+    <link rel="icon" type="image/x-icon" href="table-tree-svgrepo-com.svg">
     <style>
       @import url("styles/variables.css");
       @import url("styles/table.css");
